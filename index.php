84516501fd44dce24fa2b6301ade809d0c55ab70
kuplecikukla <101403769+kuplecikukla@users.noreply.github.com>
2022-04-09 17:52:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<!doctype html>
 <html>
-  <head>
-    <title>This is the title of the webpage!</title>
-  </head>
-  <body>
-    <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
-  </body>
+ <head>
+  <title>PHP Denemesi</title>
+ </head>
+ <body>
+ <?php echo '<p>Merhaba Dunya</p>'; ?>
+ </body>
 </html>
